9f674194b0dacc8acd81a2bba9e3281ecc029b31
ofaruksevinc <fernandomuslera125@gmail.com>
2023-05-05 11:24:15 +0300

    02.27

--- part02-Part02_27.NumberUno/src/main/java/NumberUno.java
+++ part02-Part02_27.NumberUno/src/main/java/NumberUno.java
@@ -1,11 +1,12 @@
 
-import java.util.Scanner;
-
 public class NumberUno {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        int uno = numberUno();
+        System.out.println("Uno: " + uno);
     }
 
+    public static int numberUno() {
+        return 1;
+    }
 }
