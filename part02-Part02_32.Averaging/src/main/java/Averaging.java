77177d28ed5e9307dc4393ad5c16f89abe21e499
ofaruksevinc <fernandomuslera125@gmail.com>
2023-05-05 16:00:33 +0300

    02.32

--- part02-Part02_32.Averaging/src/main/java/Averaging.java
+++ part02-Part02_32.Averaging/src/main/java/Averaging.java
@@ -3,13 +3,17 @@ public class Averaging {
 
     // implement the sum method here again
     public static int sum(int number1, int number2, int number3, int number4) {
+        
+        int sum = number1 + number2 + number3 +number4;
         // write some code here
-        return 0;
+        return sum;
     }
 
     public static double average(int number1, int number2, int number3, int number4) {
-        //write some code here
-        return -1;
+            
+        double sums = number1 + number2 + number3 + number4;
+        double avr = 1.0 * sums/4;
+        return avr;
     }
 
     public static void main(String[] args) {
