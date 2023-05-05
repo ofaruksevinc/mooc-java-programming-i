93a894d669669b45fdabeb41faf70b71922b4cd9
ofaruksevinc <fernandomuslera125@gmail.com>
2023-05-05 15:40:59 +0300

    02.31

--- part02-Part02_31.Greatest/src/main/java/Greatest.java
+++ part02-Part02_31.Greatest/src/main/java/Greatest.java
@@ -2,13 +2,20 @@
 public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
-        //write some code here
+        int greatestNumber = number1;
 
-        return -1;
+        if (number2 > greatestNumber) {
+            greatestNumber = number2;
+        }
+        if (number3 > greatestNumber) {
+            greatestNumber = number3;
+        }
+
+        return greatestNumber;
     }
 
     public static void main(String[] args) {
-        int result = greatest(2, 7, 3);
+        int result = greatest(2, -9, 3);
         System.out.println("Greatest: " + result);
     }
 }
